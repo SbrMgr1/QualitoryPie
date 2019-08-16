0dae1f64ff81432317a1c5ff651a2ada91890da6
Sbr <sbr.mgr1@gmail.com>
2019-08-16 08:10:33 +0545

    Broadcasting message to other activity by user LocalBroadCastManager

--- src/main/java/com/qualitorypie/qualitorypie/Activities/BaseActivity.java
+++ src/main/java/com/qualitorypie/qualitorypie/Activities/BaseActivity.java
@@ -1,9 +1,36 @@
 package com.qualitorypie.qualitorypie.Activities;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.qualitorypie.qualitorypie.Services.MyFirebaseMessagingService;
 
 
 public class BaseActivity extends AppCompatActivity {
+    private final String TAG = "BaseActivity";
 
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcast_receiver, new IntentFilter(MyFirebaseMessagingService.FCM_ACTION));
+    }
+
+
+    public BroadcastReceiver broadcast_receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent != null) {
+                Log.d(TAG,"message received from local broadcaster");
+            }
+        }
+    };
 }
--- src/main/java/com/qualitorypie/qualitorypie/Services/MyFirebaseMessagingService.java
+++ src/main/java/com/qualitorypie/qualitorypie/Services/MyFirebaseMessagingService.java
@@ -1,33 +1,39 @@
 package com.qualitorypie.qualitorypie.Services;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-import com.qualitorypie.qualitorypie.R;
-
-import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    public static final String  FCM_ACTION = "com.qualitorypie.qualitorypie.Services.CLOUD_MESSAGING";
+    private String SENDER_TAG = "fierbase-local-sender";
+    private LocalBroadcastManager broadcaster;
+
+    @Override
+    public void onCreate() {
+        broadcaster = LocalBroadcastManager.getInstance(this);
+
+    }
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Log.d(SENDER_TAG, "Broadcasting message");
+        Intent intent = new Intent(FCM_ACTION);
+        // intent.putExtra("key", "Sample Data");
+        // put your all data using put extra
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
         super.onMessageReceived(remoteMessage);
 
 
     }
 
     @Override
-    public void onNewToken(String s) {
-        super.onNewToken(s);
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.d(SENDER_TAG, "Refreshed token: " + token);
     }
 }
